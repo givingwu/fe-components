de71c641862e438f11c13c2cc339ff528710b9ef
Chan Wu <givingwu@gmail.com>
2019-09-03 11:14:40 +0800

    fix: typo

--- src/react/components/BMap/index.js
+++ src/react/components/BMap/index.js
@@ -1,6 +1,6 @@
 /**
  * Inspired from [`react-bmap`](https://github.com/huiyan-fe/react-bmap)
- * 1. Does not install any Unusable and Complex dependecies.
+ * 1. Does not need to install any Unusable and Complex dependecies.
  * 2. Let Driving Route supports wayPoints.
  * 3. Lazyload BMap lib JS SDK.
  * 4. Only keep four components.
